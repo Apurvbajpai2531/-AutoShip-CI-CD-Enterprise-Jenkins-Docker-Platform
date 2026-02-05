e26e5cdf56f12f6078dbc1983ba2807d5a8b262e
Apurv Bajpai <154694855+Apurvbajpai2531@users.noreply.github.com>
2026-02-05 16:38:17 +0530

    Change server port from 3000 to 3001

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, async () => {
   try {
